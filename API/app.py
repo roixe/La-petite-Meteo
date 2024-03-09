138a1382aa27fe8d2146f3a1242234ca1756149e
eliott <eliott.cheminal@viacesi.fr>
2024-03-09 16:54:19 +0100

    Add /sonde and /releve

--- API/app.py
+++ API/app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template ,request
-from flask_mysqldb import MySQL
+from flask import Flask, request, render_template
+import mariadb
 from datetime import datetime
- 
+
+conn = mariadb.connect(
+    host="127.0.0.1",
+    port=3307,
+    user="root",
+    password='',
+    database="la-petite-meteo"
+)
+cur = conn.cursor()
+    
 app = Flask(__name__)
  
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'la-petite-meteo'
-
-mysql = MySQL(app)
-
-@app.route('/index')
-def home():
-    return render_template('index.html')
+@app.route('/')
+def index():
+  return render_template("index.html")
 
 @app.route('/sonde', methods = ['GET', 'POST'])
 def sonde():
@@ -21,17 +23,14 @@ def sonde():
         try :
             name = request.args['name']
             zone = request.args['zone']
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' INSERT INTO sonde VALUES('', %s,%s)''',(name,zone))
-            mysql.connection.commit()
-            cursor.close()
+            cur.execute(''' INSERT INTO sonde (nom, zone) VALUES(%s,%s)''',(name,zone))
         except KeyError:
             return "Please specify name and zone"
-        return f"Done!!"    
-    cursor = mysql.connection.cursor()
-    cursor.execute(""" SELECT * FROM sonde """)
-    rv = cursor.fetchall()
-    return str(rv)
+        return f"Done!!"  
+    else:
+        cur.execute(''' SELECT * FROM sonde ''')
+        return cur.fetchall()
+    
 
 @app.route('/releve', methods = ['GET', 'POST'])
 def releve():
@@ -41,16 +40,12 @@ def releve():
             humidity = request.args['humidity']
             date = datetime.now()
             sonde = request.args['sonde']
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' INSERT INTO releve VALUES('', %s,%s,%s,%s)''',(temperature,humidity,date,sonde))
-            mysql.connection.commit()
-            cursor.close()
+            cur.execute(''' INSERT INTO releve (temperature, humidite, date, id_sonde) VALUES(%s,%s,%s,%s)''',(temperature,humidity,date,sonde))
         except KeyError:
             return "Missing information"
         return f"Done!!"
-    cursor = mysql.connection.cursor()
-    cursor.execute(""" SELECT * FROM releve """)
-    rv = cursor.fetchall()
-    return str(rv)
+    else:
+        cur.execute(''' SELECT * FROM releve ''')
+        return cur.fetchall()
 
 app.run(host='localhost', port=5000)
