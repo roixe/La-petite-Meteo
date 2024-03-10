9f61800708eac7d4fdf854734cdc369d77967de7
gaellerem <146330927+gaellerem@users.noreply.github.com>
2024-03-10 22:33:20 +0100

    maj code app.py

--- API/app.py
+++ API/app.py
@@ -2,6 +2,15 @@ from flask import Flask, request, render_template
 import mariadb
 from datetime import datetime
 
+""" essai local pc
+conn = mariadb.connect(
+    host="localhost",
+    port=3307,
+    user="root",
+    password='',
+    database="lapetitemeteo"
+)"""
+
 conn = mariadb.connect(
     host="localhost",
     user="www-data",
@@ -24,7 +33,6 @@ def sonde():
             name = request.args['name']
             zone = request.args['zone']
             cur.execute(''' INSERT INTO sonde (nom, zone) VALUES(%s,%s)''',(name,zone))
-            cur.execute(''' INSERT INTO sonde (nom, zone) VALUES(%s,%s)''',(name,zone))
         except KeyError:
             return "Please specify name and zone"
         return f"Done!!"  
@@ -42,7 +50,6 @@ def releve():
             humidity = request.args['humidity']
             date = datetime.now()
             sonde = request.args['sonde']
-            cur.execute(''' INSERT INTO releve (temperature, humidite, date, id_sonde) VALUES(%s,%s,%s,%s)''',(temperature,humidity,date,sonde))
             cur.execute(''' INSERT INTO releve (temperature, humidite, date, id_sonde) VALUES(%s,%s,%s,%s)''',(temperature,humidity,date,sonde))
         except KeyError:
             return "Missing information"
