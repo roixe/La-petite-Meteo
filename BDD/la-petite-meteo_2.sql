7a96b5c61ea24b229012e1dae02bf218560da58d
Norlik13 <louis.garagnon@viacesi.fr>
2024-03-25 11:24:21 +0100

    [DATABASE]
    Ajout de MAC_adress dans la table sonde

--- BDD/la-petite-meteo_2.sql
+++ BDD/la-petite-meteo_2.sql
@@ -1,22 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 15 fév. 2024 à 12:34
--- Version du serveur : 8.2.0
--- Version de PHP : 8.2.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de données : `la-petite-meteo`
 --
@@ -36,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `releve` (
   `ID_sonde` int NOT NULL,
   PRIMARY KEY (`ID_releve`),
   KEY `ID_sonde` (`ID_sonde`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 -- --------------------------------------------------------
@@ -50,10 +31,6 @@ CREATE TABLE IF NOT EXISTS `sonde` (
   `ID_sonde` int NOT NULL AUTO_INCREMENT,
   `Nom` text NOT NULL,
   `Zone` text NOT NULL,
+  `MAC_adress` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`ID_sonde`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
