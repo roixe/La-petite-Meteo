2b47c4ef5730b3cbce633805334b36e92814cd81
Gaelle <146330927+gaellerem@users.noreply.github.com>
2024-03-25 12:13:39 +0100

    Update la-petite-meteo_2.sql

--- BDD/la-petite-meteo_2.sql
+++ BDD/la-petite-meteo_2.sql
@@ -1,24 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 15 fév. 2024 à 12:34
--- Version du serveur : 8.2.0
--- Version de PHP : 8.2.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `la-petite-meteo_2`
+-- Base de données : `la-petite-meteo`
 --
 
 -- --------------------------------------------------------
@@ -35,9 +16,9 @@ CREATE TABLE IF NOT EXISTS `releve` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ID_sonde` int NOT NULL,
   PRIMARY KEY (`ID_releve`),
-  KEY `ID_sonde` (`ID_sonde`),
-  CONSTRAINT `fk_releve_sonde` FOREIGN KEY (`ID_sonde`) REFERENCES `sonde` (`ID_sonde`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `ID_sonde` (`ID_sonde`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 -- --------------------------------------------------------
 
@@ -50,10 +31,6 @@ CREATE TABLE IF NOT EXISTS `sonde` (
   `ID_sonde` int NOT NULL AUTO_INCREMENT,
   `Nom` text NOT NULL,
   `Zone` text NOT NULL,
+  `MAC_address` text NOT NULL,
   PRIMARY KEY (`ID_sonde`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
