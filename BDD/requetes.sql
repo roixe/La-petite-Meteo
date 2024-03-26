1e80a1d93e2d902d4cb12f28f06fc1620d992378
gaellerem <146330927+gaellerem@users.noreply.github.com>
2024-03-26 11:41:06 +0100

    update affichage dernier relevé

--- BDD/requetes.sql
+++ BDD/requetes.sql
@@ -3,3 +3,19 @@ GRANT ALL PRIVILEGES ON *.* to 'www-data'@'localhost' IDENTIFIED BY 'www-data';
 
 ALTER TABLE sonde ADD MAC_address text NOT NULL;
 
+/* Récupérer les derniers relevés pour chaque sonde */
+SELECT date, temperature, humidite, ID_sonde FROM releve
+WHERE (ID_sonde, date) IN
+    (
+        SELECT ID_sonde, MAX(date)
+        AS max_date FROM releve
+        GROUP BY ID_sonde
+    )
+;
+
+/* Récupérer les derniers relevés des dernières 48 heures par sondes*/
+SELECT date, temperature, humidite 
+FROM releve 
+WHERE date BETWEEN DATE_SUB(NOW(), INTERVAL 48 HOUR) AND NOW() 
+AND ID_sonde = {sonde[0]}
+ORDER BY date DESC;
