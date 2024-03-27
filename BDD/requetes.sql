f8c6dcf3ca21ab8e305ccc5ce0ac4c904ea6f1c6
gaellerem <146330927+gaellerem@users.noreply.github.com>
2024-03-27 08:58:55 +0100

    update affichage dernier releve

--- BDD/requetes.sql
+++ BDD/requetes.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE lapetitemeteo;
 GRANT ALL PRIVILEGES ON *.* to 'www-data'@'localhost' IDENTIFIED BY 'www-data';
 
-ALTER TABLE sonde ADD MAC_address text NOT NULL;
+ALTER TABLE sonde ADD COLUMN MAC_address text NOT NULL;
 
 /* Récupérer le derniers relevé pour chaque sonde */
 SELECT date, temperature, humidite, ID_sonde FROM releve
@@ -19,3 +19,6 @@ FROM releve
 WHERE date BETWEEN DATE_SUB(NOW(), INTERVAL 48 HOUR) AND NOW() 
 AND ID_sonde = {sonde[0]}
 ORDER BY date DESC;
+
+/* Récupérer la liste des relevés avec le nom de la sonde plutôt que son ID*/
+SELECT r.date, r.temperature, r.humidite, s.nom AS nom_sonde FROM releve r JOIN sonde s ON r.ID_sonde = s.ID_sonde;
